e93086bf5ad1e61009c80fcc9537de75d46e4ec0
Emma <115744148+emmalelele@users.noreply.github.com>
2023-11-23 11:16:03 -0600

    making the player and some dots obstacles that can collide with each other

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let theSky;
 let y1;
 let y2;
 let scrollSpeed = 3;
+let mainBall;
+let gems;
 
 
 function preload(){
@@ -12,13 +14,37 @@ function setup() {
 	new Canvas(windowWidth, windowHeight);
   y1 = 50;
   y2 = -550;
+  makePlayer()
+  dotsObstacle();
 
 	
 }
 
 function draw() {
   moveBackground()
+  mainBall.moveTowards(mouse);
+  
 }
+
+
+function makePlayer() {
+  noStroke();
+  mainBall = new Sprite(width / 2, height / 2, 50, "lavender");
+  
+}
+
+
+function dotsObstacle() {
+  gems = new Group();
+  gems.diameter = 10;
+  gems.x = () => random(0, width);
+  gems.y = ()  => random(0, width);
+  gems.amount = 200;
+}
+
+
+
+
 
 //make scrolling background
 function moveBackground() {
