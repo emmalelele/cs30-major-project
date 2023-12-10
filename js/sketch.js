1ce79e468c0bcceb9b7d3a7cf2b9ffce6364e80c
emmalelele <thaonhat344@gmail.com>
2023-12-10 16:15:03 -0600

    clean up the code, made some change for displayObstacle function
    i clean up my code so it looks more readable, also separated the displayObstacle function to 2 scenarios so it can display all the obstacle that I want

--- js/sketch.js
+++ js/sketch.js
@@ -1,62 +1,92 @@
+
+//defining variables
 let theSky;
-let y1;
-let y2;
-let scrollSpeed = 3;
+let backgroundY1;
+let backgroundY2;
+let scrollSpeed = 4;
 let mainBall;
 let gems =[];
 let gem2 = [];
 let gem3 = [];
 let obstacleArr = [];
 let lastGemTime = 0;
- let balloon;
+let balloon;
+let theBirdImage;
+let birdObstacle;
+let birdGroup;
+let imageScale = 0.5
+let score = 0
 
 
- // class Obstacle(){
-
- // }
-
-
-
-
-
-
- function preload(){
+//loading images
+function preload(){
   theSky = loadImage("sky.jpg")
+ 
 }
+
+//set up
 function setup() {
 	new Canvas(windowWidth, windowHeight);
-  y1 = 50;
-  y2 = -550;
+  backgroundY1 = 0;
+  backgroundY2 = height;
   makePlayer()
   makeBalloon()
-  dotsObstacle();
+ 
+  
   
 }
+
+//draw 
 function draw() {
-  moveBackground()
+  //a timer to check which kind of obstacle to display
+  score ++;
+  scrollingDownBackground();
+
+  //removing off-screen obstacle
   removeOffscreenObstacles(gems);
   removeOffscreenObstacles(gem2);
   removeOffscreenObstacles(gem3);
-  mainBall.moveTowards(mouse);
-  scrollingObstacle(gems)
-  displayObstacles()
-  
-  }
 
-  function displayObstacles() {
-    if (frameCount - lastGemTime > 4 * 60) {
-      if (frameCount % 4 === 0) {
+  //making the shield move toward mouse
+  mainBall.moveTowards(mouse);
+
+  //update the position of the obstacles
+  updateGem()
+
+  console.log(score)
+  
+
+//displaying different scenario
+  if (score < 1000){
+    displayObstacles();
+  }
+  else if (score > 1000){
+    displayObstacleScenario2()
+  }
+}
+
+// scenario 1
+function displayObstacles() {
+    if (frameCount - lastGemTime > 4 * 60) { //make new obstacle every 4 seconds
+      if (frameCount % 3 === 0) {
         makeGemSquare(100, 100, () => random(0, width), 0, 4);
       } 
-      else if (frameCount % 4 === 1) {
-        makeGemSquare(50, 10, () => random(0, width), 0, 100);
+      else if (frameCount % 3=== 1) {
+        makeGemSquare(random(20,70), random(20,70), () => random(0, width), 0, 100);
         
       } 
-      else if (frameCount % 4 === 2) {
-        makeGemRect(300, 10, width/2, 0, 5)
+      else if (frameCount % 3 === 2) {
+        makeGemRect(600, 10, width/2, 0, 5)
 
       } 
-      else if (frameCount % 3 === 0) {
+      lastGemTime = frameCount;
+    } 
+  }
+  
+//scenario 2
+function displayObstacleScenario2(){
+    if (frameCount - lastGemTime > 2 * 60){ // make new obstacle every 2 seconds
+      if (frameCount % 3 === 0) {
         dotsObstacle(10, 10)
       } 
       else if (frameCount % 3 === 1) {
@@ -65,32 +95,13 @@ function draw() {
       else if (frameCount % 3 === 2 ) {
         dotsObstacle(100, () => random(0, width), 0, 5)
       } 
-      else {
-        dotsObstacle(70, () => random(0, width), 0, 10)
-      }
       lastGemTime = frameCount;
     }
-    updateGem();
-    if (checkCollide(balloon, mainBall)) {
-      balloon.allowSleeping = true;
-    }
+   
   }
-  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function to update position of the obstacles
 function updateGem(){
     //update and draw the gems in gem3 group
     if (gems) {
@@ -107,14 +118,27 @@ function updateGem(){
         scrollingObstacle(gem3)
       }
   }
+
+
  
-  
+//making the shield
 function makePlayer() {
   noStroke();
   mainBall = new Sprite(width / 2, height / 2, 50, "lavender");
   
 }
 
+//make the balloon
+function makeBalloon(){
+  balloon = createSprite(width / 2, height / 2 + 50, 50);
+  balloon.collider = 'static'
+}
+
+
+////////////////////////////////////////-----------------------------------------------/////////////////////////////////////////////
+
+
+// circle obstacle
 function dotsObstacle(diameter, x, y, amount) {
   gems = new Group();
   gems.diameter = diameter;
@@ -124,6 +148,7 @@ function dotsObstacle(diameter, x, y, amount) {
   obstacleArr.push(gems)
 }
 
+// square obstacles
 function makeGemSquare(width, height, x, y, amount) {
   gem2 = new Group();
   gem2.width = width;
@@ -131,10 +156,11 @@ function makeGemSquare(width, height, x, y, amount) {
   gem2.x = x;
   gem2.y = y;
   gem2.amount = amount;
-  gem2.rotationSpeed = 1
+  gem2.rotationSpeed = 10
   obstacleArr.push(gem2)
 }
 
+//rectangle obstacles
 function makeGemRect(width, height, x, y, amount) {
   gem3 = new Group();
   gem3.width = width;
@@ -142,37 +168,59 @@ function makeGemRect(width, height, x, y, amount) {
   gem3.x = x;
   gem3.y = y;
   gem3.amount = amount;
-  while (gem3.length < 9){
+  while (gem3.length < 15){
   let newGem = new gem3.Sprite();
-  newGem.y = gem3.length * 10;
+  newGem.y = gem3.length * 20;
   }
 }
 
-//make scrolling background
-function moveBackground() {
-  image(theSky, 0, y1, windowWidth, windowHeight);
-  image(theSky, 0, y2, windowWidth, windowHeight);
-  y1 += scrollSpeed;
-  y2 += scrollSpeed;
-  if (y1 >= windowHeight) {
-    y1 = -500;
+// function makeBirdObstacle(){
+//   birdObstacle = new Sprite();
+//   birdObstacle.draw = () => {
+// 		image(theBirdImage, width/2, height/2, theBirdImage.width*imageScale, theBirdImage.height*imageScale);
+// 	};
+//   birdObstacle.update= () => {
+// 		birdObstacle.moveTowards(mouse, 0.07);
+// 	};
+
+// }
+
+
+
+
+
+/////////////////////////////////////////////////////-------------------------------------//////////////////////////////////////////////////////////
+
+
+
+//Make the scrolling-down background
+function scrollingDownBackground() {
+  image(theSky, 0, backgroundY1, windowWidth, windowHeight);
+  image(theSky, 0, backgroundY2, windowWidth, windowHeight);
+  backgroundY1 += scrollSpeed;
+  backgroundY2 += scrollSpeed;
+  if (backgroundY1 >= height) {
+    backgroundY1 = -height;
   }
-  if (y2 >= windowHeight) {
-    y2 = -500;
+  if (backgroundY2 >= height) {
+    backgroundY2 = -height;
   }
 }
+
 function scrollingObstacle(gemGroup) {
   for (let i = 0; i < gemGroup.length; i++) {
     gemGroup[i].position.y += scrollSpeed;
   }
 }
 
-
+// function to remove off screen obstacles
 function removeOffscreenObstacles(gemGroup) {
   let obstacleToRemove = [];
   for (let i = gemGroup.length - 1; i >= 0; i--) {
-    if (gemGroup[i].position.x + gemGroup[i].width / 2 < 0 || gemGroup[i].position.x - gemGroup[i].width / 2 > width ||gemGroup[i].position.y + gemGroup[i].height / 2 < 0 ||gemGroup[i].position.y - gemGroup[i].height / 2 > height) {
-      console.log("Removing gem at index:", i);
+    if (gemGroup[i].position.x + gemGroup[i].width / 2 < 0 || 
+       gemGroup[i].position.x - gemGroup[i].width / 2 > width ||
+       gemGroup[i].position.y + gemGroup[i].height / 2 < 0 ||
+       gemGroup[i].position.y - gemGroup[i].height / 2 > height) {
       //so it won't mess up the index
       obstacleToRemove.push(i);
     }
@@ -182,15 +230,16 @@ function removeOffscreenObstacles(gemGroup) {
     gemGroup[i].remove();
   }
 }
+
+//check collide
 function checkCollide(balloon, obstacle){
   if (balloon.colliding(obstacle)){
     return true;
   }
 }
-function makeBalloon(){
-  balloon = createSprite(width / 2, height / 2 + 50, 50);
-  balloon.collider = 'static'
-}
+
+
+
 // function manageGameOver(){
 //   for (let i = 0; i < obstacleArr; i ++){
 //     if (checkCollide(balloon, obstacleArr)){
